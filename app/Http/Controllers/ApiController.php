819694e4408903c04a75dbd51d9b0d462ae3382d
Sergey Posunko <serg777sp@mail.ru>
2017-04-09 09:05:58 +0300

    added caching

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,15 +5,21 @@ namespace App\Http\Controllers;
 //use Illuminate\Http\Request;
 use LaravelDoctrine\ORM\Facades\EntityManager;
 use App\Models\Doc2\User;
+use Cache;
 
 class ApiController extends Controller
 {
     //
     public function getInfo($name) {
-        $user = EntityManager::getRepository(User::class)->findOneBy(['name' => $name]);
 
-        if(!$user) return "User with name - '" . $name . "' not found.";
+        if(Cache::has('info_'. $name)){
+            return Cache::get('info_'. $name);
+        } else {
+            $user = EntityManager::getRepository(User::class)->findOneBy(['name' => $name]);
+            if(!$user) return "User with name - '" . $name . "' not found.";
 
-        return $user->getInfo();
+            Cache::add('info_'.$name, $user->getInfo(), 1);
+            return $user->getInfo();
+        }
     }
 }
